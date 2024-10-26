54c911456c070c30418c7a270a74e0b68ede4b9f
riajames <riajames.17@gmail.com>
2024-10-26 13:11:36 -0500

    frontend setup

---
+++ client/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDPeOiBVaJGR5K0xFIEAyEszi3zypPXeFU",
+    authDomain: "cashcompass-d2928.firebaseapp.com",
+    projectId: "cashcompass-d2928",
+    storageBucket: "cashcompass-d2928.appspot.com",
+    messagingSenderId: "974071123465",
+    appId: "1:974071123465:web:b0a94cd70c4e865eaff2d8",
+    measurementId: "G-EBE4RC4JGD"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
