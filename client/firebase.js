fd2dfa8d717ff0eb99d222c31193802eb73bd4b4
riajames <riajames.17@gmail.com>
2024-10-26 13:35:36 -0500

    env file stuff

--- client/firebase.js
+++ client/firebase.js
@@ -1,20 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "@firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDPeOiBVaJGR5K0xFIEAyEszi3zypPXeFU",
-    authDomain: "cashcompass-d2928.firebaseapp.com",
-    projectId: "cashcompass-d2928",
-    storageBucket: "cashcompass-d2928.appspot.com",
-    messagingSenderId: "974071123465",
-    appId: "1:974071123465:web:b0a94cd70c4e865eaff2d8",
-    measurementId: "G-EBE4RC4JGD"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const firestore = getFirestore(app);
